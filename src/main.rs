b7de38792fd7c239f7dbbdc30789a4c169f4b45b
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2020-05-04 12:43:41 +0200

    Read program from file if an argument is provided

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,34 @@ use crate::program::Program;
 extern crate either;
 
 fn main() {
-    let mut p = Program::new_from_stdin();
-    match p.eval() {
+    let mut args = std::env::args();
+    let command = args.next();
+
+    let result = match args.next() {
+        Some(s) if &s == "-h" || &s == "--help"  => {
+            print_help(&command.unwrap_or(String::from("nickel")));
+            return;
+        }
+        Some(file) => {
+            match Program::new_from_file(std::path::Path::new(file.as_str())) {
+                Ok(mut p) => p.eval(),
+                Err(err) => {
+                    eprintln!("Cannot read file {}: {}", file, err);
+                    std::process::exit(1)
+                }
+            }
+        }
+        None => Program::new_from_stdin().eval()
+    };
+
+    match result {
         Ok(t) => println!("Evaluation finished with result:\n{:?}", t),
         Err(s) => println!("Evaluation didn't finished, found error:\n{}", s),
     }
+}
+
+fn print_help(command: &String) {
+    println!("Usage: {} [FILE] [-h | --help]", command);
+    println!("Execute a Nickel program. If no argument is provided, the program is read from the \
+             standard input.");
 }
