12016691b8255ab8427e8cbb57fa22d62aff4a18
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2020-10-05 10:39:44 +0200

    Add Display impl for Ident

--- src/identifier.rs
+++ src/identifier.rs
@@ -1,3 +1,12 @@
 //! Define the type of an identifier.
+use std::fmt;
+
 #[derive(Debug, Eq, Hash, PartialEq, Clone)]
 pub struct Ident(pub String);
+
+impl fmt::Display for Ident {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let Ident(ident) = self;
+        write!(f, "{}", ident)
+    }
+}
