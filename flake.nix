31de922fe593721843f7b2b146257a047a4bf2e9
Rok Garbas <rok@garbas.si>
2021-01-15 14:16:59 +0100

    use nixpkgs-mozilla and provide a way to build on different channels

--- flake.nix
+++ flake.nix
@@ -1,30 +1,53 @@
 {
   inputs.nixpkgs.url = "nixpkgs/nixos-20.09";
+  inputs.nixpkgs-mozilla.url = "github:garbas/nixpkgs-mozilla/flake";
   inputs.import-cargo.url = "github:edolstra/import-cargo";
 
-  outputs = { self, nixpkgs, import-cargo }:
+  outputs = { self, nixpkgs, nixpkgs-mozilla, import-cargo }:
     let
 
-      systems = [ "x86_64-linux" "x86_64-darwin" ];
+      CHANNELS = {
+        nightly =
+          { channel = "nightly";
+            date = "2021-01-15";
+            sha256 = "sha256-uoGBMgGmIPj4E+jCY8XH41Ia8NbaRjDC3KioiaCA/M8=";
+          };
+        beta =
+          { channel = "beta";
+            date = "2021-01-15";
+            sha256 = "sha256-DE2l5X02z9o4z8E5bmBG4QXWOAKEStdVmjZdGZE7PAQ=";
+          };
+        stable =
+          { channel = "stable";
+            date = "2020-12-31";
+            sha256 = "sha256-KCh2UBGtdlBJ/4UOqZlxUtcyefv7MH1neoVNV4z0nWs=";
+          };
+      };
+      SYSTEMS = [ "x86_64-linux" "x86_64-darwin" ];
 
-      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+      inherit (nixpkgs.lib) genAttrs substring;
 
-      buildPackage = { isShell, system }:
+      forAllSystems = f: genAttrs SYSTEMS (system: f system);
+
+      buildPackage = { system, isShell ? false, channel ? null }:
         let
-          pkgs = import nixpkgs { inherit system; };
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ nixpkgs-mozilla.overlays.rust ];
+          };
 
-          inherit (pkgs) stdenv lib;
-        in
-        stdenv.mkDerivation {
-          name = "nickel-${lib.substring 0 8 self.lastModifiedDate}-${self.shortRev or "dirty"}";
+          rust =
+            (pkgs.rustChannelOf CHANNELS."${channel}").rust.override({
+              # targets = [];
+              # extentions = [];
+            });
+
+        in pkgs.stdenv.mkDerivation {
+          name = "nickel-${substring 0 8 self.lastModifiedDate}-${self.shortRev or "dirty"}";
 
           buildInputs =
-            with pkgs; [ rustc
-              cargo
-            ] ++ (if isShell then with pkgs;[
-              rustfmt
-              clippy
-            ] else [
+            [ rust
+            ] ++ (if isShell then [] else [
               (import-cargo.builders.importCargo {
                 lockFile = ./Cargo.lock;
                 inherit pkgs;
@@ -48,14 +71,17 @@
         };
 
     in {
-
-      defaultPackage = forAllSystems (system: buildPackage { inherit system; isShell = false; });
-
-      checks = forAllSystems (system: {
-        build = self.defaultPackage.${system};
-      });
-
+      defaultPackage = forAllSystems (system: buildPackage { inherit system; });
       devShell = forAllSystems (system: buildPackage { inherit system; isShell = true; });
-
+      checks =
+        forAllSystems (system:
+          builtins.listToAttrs
+            (map (channel:
+              { name = "build_${channel}";
+                value = buildPackage { inherit system channel; };
+              }
+            )
+            (builtins.attrNames CHANNELS))
+        );
     };
 }
