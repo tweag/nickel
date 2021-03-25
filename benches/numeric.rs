768df15e97503ac3cb3973595ba1997ec4bab265
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2021-03-25 20:10:33 +0100

    Renaming

--- benches/numeric.rs
+++ benches/numeric.rs
@@ -1,29 +1,29 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
 mod common;
-use common::{call_bench, EvalMode};
+use common::{bench, EvalMode};
 
 fn fibonacci(c: &mut Criterion) {
-    call_bench("fibonacci 5", "numeric/fibonacci", None, 10, EvalMode::Normal, c);
+    bench("fibonacci 10", "numeric/fibonacci", None, 10, EvalMode::Normal, c);
 }
 
 // Waiting for nums stdlib
 // fn pidigits(c: &mut Criterion) {
-//     call_bench("pidigits 5", "numeric/pidigits", None, 10, EvalMode::Normal, c);
+//     bench("pidigits 5", "numeric/pidigits", None, 10, EvalMode::Normal, c);
 // }
 
 // Waiting for lists stdlib
 //fn sum(c: &mut Criterion) {
-//    call_bench("sum 100", "numeric/reduce", Some("sum"), 100, EvalMode::Normal, c);
+//    bench("sum 100", "numeric/reduce", Some("sum"), 100, EvalMode::Normal, c);
 //}
 //
 //fn product(c: &mut Criterion) {
-//    call_bench("product 100", "numeric/reduce", Some("product"), 100, EvalMode::Normal, c);
+//    bench("product 100", "numeric/reduce", Some("product"), 100, EvalMode::Normal, c);
 //}
 
 // Waiting for lists stdlib
 //fn scalar(c: &mut Criterion) {
-//    call_bench("scalar 100", "numeric/scalar", None, 100, EvalMode::Normal, c);
+//    bench("scalar 100", "numeric/scalar", None, 100, EvalMode::Normal, c);
 //}
 
 //criterion_group!(benches, fibonacci, pidigits, sum, product, scalar);
