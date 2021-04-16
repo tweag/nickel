9e4f6a7274b9d0dd50c1c2a12426dd41f2d620ba
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2021-04-16 11:38:23 +0200

    Enable all benches

--- benches/lists.rs
+++ benches/lists.rs
@@ -2,37 +2,241 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 use utilities::{bench, EvalMode};
 
-fn fold_normal(c: &mut Criterion) {
-    bench("fold normal 100", env!("CARGO_MANIFEST_DIR"), "lists/fold", Some("right.strings"), 100, EvalMode::Normal, c);
+fn fold_strings(c: &mut Criterion) {
+    bench(
+        "foldr strings 50",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/fold",
+        Some("right.strings"),
+        50,
+        EvalMode::Normal,
+        c,
+    );
 }
 
-fn fold_deepseq(c: &mut Criterion) {
-    bench("fold deepseq 100", env!("CARGO_MANIFEST_DIR"), "lists/fold", Some("right.strings"), 100, EvalMode::DeepSeq, c);
+fn fold_strings_deep(c: &mut Criterion) {
+    bench(
+        "foldr deepseq strings 30",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/fold",
+        Some("right.strings"),
+        30,
+        EvalMode::DeepSeq,
+        c,
+    );
+}
+
+fn fold_nums(c: &mut Criterion) {
+    bench(
+        "foldr nums 50",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/fold",
+        Some("right.nums"),
+        50,
+        EvalMode::Normal,
+        c,
+    );
+}
+
+fn fold_nums_deep(c: &mut Criterion) {
+    bench(
+        "foldr deepseq nums 30",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/fold",
+        Some("right.nums"),
+        30,
+        EvalMode::DeepSeq,
+        c,
+    );
+}
+
+fn fold_lists(c: &mut Criterion) {
+    bench(
+        "foldr lists 50",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/fold",
+        Some("right.lists_"),
+        50,
+        EvalMode::Normal,
+        c,
+    );
+}
+
+fn fold_lists_deep(c: &mut Criterion) {
+    bench(
+        "foldr deepseq lists 30",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/fold",
+        Some("right.lists_"),
+        30,
+        EvalMode::DeepSeq,
+        c,
+    );
+}
+
+fn foldl_strings(c: &mut Criterion) {
+    bench(
+        "foldl strings 50",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/fold",
+        Some("left.strings"),
+        50,
+        EvalMode::Normal,
+        c,
+    );
+}
+
+fn foldl_strings_deep(c: &mut Criterion) {
+    bench(
+        "foldl deepseq strings 30",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/fold",
+        Some("left.strings"),
+        30,
+        EvalMode::DeepSeq,
+        c,
+    );
+}
+
+fn foldl_nums(c: &mut Criterion) {
+    bench(
+        "foldl nums 50",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/fold",
+        Some("left.nums"),
+        50,
+        EvalMode::Normal,
+        c,
+    );
+}
+
+fn foldl_nums_deep(c: &mut Criterion) {
+    bench(
+        "foldl deepseq nums 30",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/fold",
+        Some("left.nums"),
+        30,
+        EvalMode::DeepSeq,
+        c,
+    );
+}
+
+fn foldl_lists(c: &mut Criterion) {
+    bench(
+        "foldl lists 50",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/fold",
+        Some("left.lists_"),
+        50,
+        EvalMode::Normal,
+        c,
+    );
+}
+
+fn foldl_lists_deep(c: &mut Criterion) {
+    bench(
+        "foldl deepseq lists 30",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/fold",
+        Some("left.lists_"),
+        30,
+        EvalMode::DeepSeq,
+        c,
+    );
 }
 
 fn generate_normal(c: &mut Criterion) {
-    bench("generate normal 50", env!("CARGO_MANIFEST_DIR"), "lists/generate", None, 50, EvalMode::Normal, c);
+    bench(
+        "generate normal 50",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/generate",
+        None,
+        50,
+        EvalMode::Normal,
+        c,
+    );
 }
 
 fn generate_deepseq(c: &mut Criterion) {
-    bench("generate deepseq 50", env!("CARGO_MANIFEST_DIR"), "lists/generate", None, 50, EvalMode::DeepSeq, c);
+    bench(
+        "generate deepseq 30",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/generate",
+        None,
+        30,
+        EvalMode::DeepSeq,
+        c,
+    );
 }
 
 fn map_normal(c: &mut Criterion) {
-    bench("map normal 100", env!("CARGO_MANIFEST_DIR"), "lists/map", None, 100, EvalMode::Normal, c);
+    bench(
+        "map normal 50",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/map",
+        None,
+        50,
+        EvalMode::Normal,
+        c,
+    );
 }
 
 fn map_deepseq(c: &mut Criterion) {
-    bench("map deepseq 100", env!("CARGO_MANIFEST_DIR"), "lists/map", None, 100, EvalMode::DeepSeq, c);
+    bench(
+        "map deepseq 30",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/map",
+        None,
+        30,
+        EvalMode::DeepSeq,
+        c,
+    );
 }
 
 fn pipe_normal(c: &mut Criterion) {
-   bench("pipe normal 50", env!("CARGO_MANIFEST_DIR"), "lists/pipe", None, 50, EvalMode::Normal, c);
+    bench(
+        "pipe normal 30",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/pipe",
+        None,
+        30,
+        EvalMode::Normal,
+        c,
+    );
 }
 
 fn pipe_deepseq(c: &mut Criterion) {
-   bench("pipe deepseq 50", env!("CARGO_MANIFEST_DIR"), "lists/pipe", None, 50, EvalMode::DeepSeq, c);
+    bench(
+        "pipe deepseq 20",
+        env!("CARGO_MANIFEST_DIR"),
+        "lists/pipe",
+        None,
+        20,
+        EvalMode::DeepSeq,
+        c,
+    );
 }
 
-criterion_group!(benches, fold_normal, fold_deepseq, generate_normal, generate_deepseq, map_normal, map_deepseq, pipe_normal, pipe_deepseq);
+criterion_group!(
+    benches,
+    fold_strings,
+    fold_strings_deep,
+    fold_nums,
+    fold_nums_deep,
+    fold_lists,
+    fold_lists_deep,
+    foldl_strings,
+    foldl_strings_deep,
+    foldl_nums,
+    foldl_nums_deep,
+    foldl_lists,
+    foldl_lists_deep,
+    generate_normal,
+    generate_deepseq,
+    map_normal,
+    map_deepseq,
+    pipe_normal,
+    pipe_deepseq
+);
 criterion_main!(benches);
--- benches/numeric.rs
+++ benches/numeric.rs
@@ -3,23 +3,63 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use utilities::{bench, EvalMode};
 
 fn fibonacci(c: &mut Criterion) {
-    bench("fibonacci 10", env!("CARGO_MANIFEST_DIR"), "numeric/fibonacci", None, 10, EvalMode::Normal, c);
+    bench(
+        "fibonacci 10",
+        env!("CARGO_MANIFEST_DIR"),
+        "numeric/fibonacci",
+        None,
+        10,
+        EvalMode::Normal,
+        c,
+    );
 }
 
 fn pidigits(c: &mut Criterion) {
-    bench("pidigits 5", env!("CARGO_MANIFEST_DIR"), "numeric/pidigits", None, 10, EvalMode::Normal, c);
+    bench(
+        "pidigits 100",
+        env!("CARGO_MANIFEST_DIR"),
+        "numeric/pidigits",
+        None,
+        100,
+        EvalMode::Normal,
+        c,
+    );
 }
 
 fn sum(c: &mut Criterion) {
-   bench("sum 100", env!("CARGO_MANIFEST_DIR"), "numeric/reduce", Some("sum"), 100, EvalMode::Normal, c);
+    bench(
+        "sum 30",
+        env!("CARGO_MANIFEST_DIR"),
+        "numeric/reduce",
+        Some("sum"),
+        30,
+        EvalMode::Normal,
+        c,
+    );
 }
 
 fn product(c: &mut Criterion) {
-   bench("product 100", env!("CARGO_MANIFEST_DIR"), "numeric/reduce", Some("product"), 100, EvalMode::Normal, c);
+    bench(
+        "product 30",
+        env!("CARGO_MANIFEST_DIR"),
+        "numeric/reduce",
+        Some("product"),
+        30,
+        EvalMode::Normal,
+        c,
+    );
 }
 
 fn scalar(c: &mut Criterion) {
-   bench("scalar 100", env!("CARGO_MANIFEST_DIR"), "numeric/scalar", None, 100, EvalMode::Normal, c);
+    bench(
+        "scalar 10",
+        env!("CARGO_MANIFEST_DIR"),
+        "numeric/scalar",
+        None,
+        10,
+        EvalMode::Normal,
+        c,
+    );
 }
 
 criterion_group!(benches, fibonacci, pidigits, sum, product, scalar);
