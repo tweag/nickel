812d47c85360838b62c06aefbd5b6598810114e5
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2021-06-29 12:51:28 +0200

    Tentative to fix missing popperjs error

--- website/gatsby-browser.js
+++ website/gatsby-browser.js
@@ -1,5 +1,6 @@
 import "./src/styles/custom.scss";
 import "jquery/dist/jquery.min.js";
-import "@popperjs/core/dist/umd/popper.min.js";
+//import "@popperjs/core/dist/umd/popper.min.js";
+import "popper.js/dist/popper.min";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "./src/styles/global.css";
