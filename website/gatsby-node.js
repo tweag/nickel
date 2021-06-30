1556726f36d71e6312c2dc7d86b1793ddcc57743
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2021-06-30 10:23:20 +0200

    Fix webassembly error with webpack

--- website/gatsby-node.js
+++ website/gatsby-node.js
@@ -2,7 +2,7 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
     actions.setWebpackConfig({
         experiments: {
             // This was necessary to have the Nickel WASM REPL work with Webpack
-            syncWebAssembly: true,
+            asyncWebAssembly: false,
         },
     })
 };
