dfa5cd0e17fef1f61bcfef0b7a97cc73f4cfc5f6
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2021-06-30 10:26:06 +0200

    Re-enable webassembly, disable compilation of modules using

--- website/gatsby-node.js
+++ website/gatsby-node.js
@@ -1,8 +1,24 @@
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
+exports.onCreateWebpackConfig = ({ stage, actions, loaders }) => {
     actions.setWebpackConfig({
         experiments: {
             // This was necessary to have the Nickel WASM REPL work with Webpack
-            asyncWebAssembly: false,
+            asyncWebAssembly: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /react-ace/,
+                    use: loaders.null(),
+                },
+                {
+                    test: /ace-builds/,
+                    use: loaders.null(),
+                },
+                {
+                    test: /ace-nickel-mode.js/,
+                    use: loaders.null(),
+                }
+            ],
         },
     })
 };
