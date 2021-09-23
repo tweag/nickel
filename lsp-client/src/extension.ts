ffb4c97d4ff4ee966891ef8065c42725f8530050
Yannik Sander <me@ysndr.de>
2021-09-23 14:04:43 +0300

    Add lsp-client vscode extension

---
+++ lsp-client/src/extension.ts
@@ -0,0 +1,70 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+	// The server is implemented in node
+	const serverModule = context.asAbsolutePath(
+		path.join('target', 'debug', 'nls')
+	);
+	// The debug options for the server
+	const debugOptions = { env: { "RUST_LOG": "trace" } };
+
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	const serverOptions: ServerOptions = {
+		run: {
+			command: context.asAbsolutePath(
+				path.join('target', 'debug', 'nls')
+			), transport: TransportKind.stdio, options: { env: { "RUST_LOG": "trace" } }
+		},
+		debug: {
+			command: context.asAbsolutePath(
+				path.join('target', 'debug', 'nls')
+			),
+			transport: TransportKind.stdio,
+			options: debugOptions
+		}
+	};
+
+	// Options to control the language client
+	const clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'nickel' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.ncl')
+		}
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'languageServerExample',
+		'Language Server Example',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
