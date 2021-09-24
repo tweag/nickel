c3c6956778f4634090de3b942796a6564046a2d7
francois-caddet <francois.caddet@icloud.com>
2021-09-24 12:46:26 +0200

    iadd tests to fail imports of bad contracts and bad typing

---
+++ tests/imports.rs
@@ -0,0 +1,30 @@
+
+use std::path::PathBuf;
+use nickel::program::Program;
+use std::io::BufReader;
+use nickel::error::{Error,EvalError,TypecheckError};
+
+fn mk_import(file: &str) -> String {
+    let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    path.push(format!("tests/imports/{}", file));
+    format!("import \"{}\"", path.into_os_string().into_string().unwrap())
+}
+
+#[test]
+fn contract_fail() {
+    let mut prog = Program::new_from_source(BufReader::new(mk_import("contract-fail.ncl").as_bytes()),"should_fail").unwrap();
+    prog.eval().map_err(|e| match e {
+        Error::EvalError(EvalError::BlameError(_,_)) => Ok(()),
+        r => Err(r),
+    }).unwrap_err().unwrap();
+}
+
+#[test]
+fn typecheck_fail() {
+    let mut prog = Program::new_from_source(BufReader::new(mk_import("typecheck-fail.ncl").as_bytes()),"should_fail").unwrap();
+    prog.eval().map_err(|e| match e {
+        Error::TypecheckError(TypecheckError::TypeMismatch(..)) => Ok(()),
+        r => Err(r),
+    }).unwrap_err().unwrap();
+}
+
