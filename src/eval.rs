259867ca6b050e5a31635a22f5f3966cfe5929c9
Litchi Pi <litchi.pi@protonmail.com>
2021-10-26 10:43:58 +0200

    Fix issue #425
    Continuating term evaluation as long as there are arguments in the stack.
    Only return metadata once every arguments are consumed by the App
    
    Signed-off-by: Litchi Pi <litchi.pi@protonmail.com>

--- src/eval.rs
+++ src/eval.rs
@@ -717,7 +717,7 @@ where
                 }
             }
             // Unwrapping of enriched terms
-            Term::MetaValue(meta) if enriched_strict => {
+            Term::MetaValue(meta) if enriched_strict || (stack.count_args() > 0) => {
                 if meta.value.is_some() {
                     /* Since we are forcing a metavalue, we are morally evaluating `force t` rather
                      * than `t` iteself.  Updating a thunk after having performed this forcing may
