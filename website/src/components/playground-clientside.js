5227ca6c214bd6a94cff9227697d797f785dc028
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2021-11-05 13:47:51 +0100

    Add a spinning icon when loading playground

--- website/src/components/playground-clientside.js
+++ website/src/components/playground-clientside.js
@@ -1,4 +1,9 @@
 import Loadable from "@loadable/component";
+import React, { Suspense } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
 
 /**
  * Wrapper around the Playground component to use it on the client side only (and not via server-side rendering).
@@ -6,4 +11,13 @@ import Loadable from "@loadable/component";
  * @type {React.ForwardRefExoticComponent<React.PropsWithoutRef<{}> & React.RefAttributes<unknown>>}
  */
 const LoadablePlayground = Loadable(() => import("./playground/playground"));
-export default LoadablePlayground
+
+export default function ClientSidePlayground(props) {
+    return (
+        <React.Suspense fallback={<FontAwesomeIcon icon={faSpinner} spin/>}>
+            <div>
+                <LoadablePlayground {...props}/>
+            </div>
+        </React.Suspense>
+    );
+}
