6d84799c104e7fe6b03431890f3de49a0d66042c
Yannik Sander <me@ysndr.de>
2021-12-03 17:21:53 +0100

    Make tracer globally accessible

--- lsp/nls/src/trace.rs
+++ lsp/nls/src/trace.rs
@@ -1,13 +1,21 @@
 use std::{
     collections::HashMap,
+    sync::{Mutex, MutexGuard},
     time::{Duration, Instant},
 };
 
 use anyhow::Result;
+use lazy_static::lazy_static;
+use lsp_server::RequestId;
 use serde_json::Value;
 
-use self::param::Param;
+use self::param::Enrichment;
 
+lazy_static! {
+    static ref TRACE: Mutex<Trace> = Mutex::new(Trace::default());
+}
+
+#[derive(Debug)]
 struct TraceItem<T> {
     time: T,
     params: TraceItemParams,
@@ -19,31 +27,48 @@ pub struct TraceItemParams {
     linearization_size: Option<usize>,
 }
 
+#[derive(Debug)]
 struct Received(Instant);
+#[derive(Debug)]
 struct Replied {
     ingress: Instant,
     duration: Duration,
     with_error: bool,
 }
 
+#[derive(Debug, Default)]
 struct Trace {
-    received: HashMap<usize, TraceItem<Received>>,
+    received: HashMap<RequestId, TraceItem<Received>>,
     replied: Vec<TraceItem<Replied>>,
 }
 
-impl Trace {
-    pub fn receive(&mut self, id: usize, method: impl ToString) {
+pub struct Tracer;
+
+impl Tracer {
+    fn with_trace<F, O>(f: F) -> anyhow::Result<O>
+    where
+        F: FnOnce(MutexGuard<Trace>) -> O,
+    {
+        TRACE
+            .lock()
+            .map(f)
+            .or_else(|_| anyhow::bail!("Could not lock tracer mutex"))
+    }
+
+    pub fn receive(id: RequestId, method: impl ToString) {
         let time = Received(Instant::now());
         let params = TraceItemParams {
             method: method.to_string().into(),
             ..Default::default()
         };
 
-        self.received.insert(id, TraceItem { time, params });
+        Tracer::with_trace(|mut trace| trace.received.insert(id, TraceItem { time, params }))
+            .unwrap();
     }
 
-    pub fn reply(&mut self, id: usize) {
-        let received = self.received.remove(&id);
+    pub fn reply(&mut self, id: RequestId) -> anyhow::Result<()> {
+        let received = Self::with_trace(|mut t| t.received.remove(&id))?;
+
         for TraceItem {
             time: Received(ingress),
             params,
@@ -53,39 +78,42 @@ impl Trace {
 
             let duration = egress - ingress;
 
-            self.replied.push(TraceItem {
-                time: Replied {
-                    ingress,
-                    duration,
-                    with_error: false,
-                },
-                params,
-            })
+            Self::with_trace(|mut t| {
+                t.replied.push(TraceItem {
+                    time: Replied {
+                        ingress,
+                        duration,
+                        with_error: false,
+                    },
+                    params,
+                })
+            })?;
         }
+        Ok(())
     }
 }
 
-trait Enrich<T: Param> {
-    fn enrich(&mut self, id: usize, param: impl Into<T>) {}
+pub(crate) trait Enrich<E: Enrichment> {
+    fn enrich(id: &RequestId, param: E) -> anyhow::Result<()>;
 }
 
-mod param {
+pub mod param {
 
-    use super::{Enrich, Trace};
-    use derive_more::From;
+    use super::{Enrich, Tracer};
+    use lsp_server::RequestId;
+    use nickel::typecheck::linearization::Completed;
 
-    pub trait Param {}
+    pub trait Enrichment {}
 
-    #[derive(From, Debug, Default)]
-    pub struct LinearizationSize(usize);
-    impl Param for LinearizationSize {}
+    impl Enrichment for &Completed {}
 
-    impl Enrich<LinearizationSize> for Trace {
-        fn enrich(&mut self, id: usize, param: impl Into<LinearizationSize>) {
-            let LinearizationSize(size) = param.into();
-            self.received.entry(id).and_modify(|item| {
-                item.params.linearization_size.insert(size);
-            });
+    impl Enrich<&Completed> for Tracer {
+        fn enrich(id: &RequestId, param: &Completed) -> anyhow::Result<()> {
+            Self::with_trace(|mut t| {
+                t.received.entry(id.to_owned()).and_modify(|item| {
+                    item.params.linearization_size.insert(param.lin.len());
+                });
+            })
         }
     }
 }
