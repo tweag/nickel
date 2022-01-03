506d7acff4d42eab3b641237f2a7c9c792770fd6
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2022-01-03 17:49:36 +0100

    Add missing mut modifier

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -577,7 +577,7 @@ where
                 }
             }
             // Unwrapping of enriched terms
-            Term::MetaValue(meta) if enriched_strict => {
+            Term::MetaValue(mut meta) if enriched_strict => {
                 if meta.value.is_some() {
                     /* Since we are forcing a metavalue, we are morally evaluating `force t` rather
                      * than `t` iteself.  Updating a thunk after having performed this forcing may
