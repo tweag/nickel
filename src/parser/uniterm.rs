9f049434a3ac0f4168f36e536c1eb94a70fc9d2b
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2022-02-08 16:13:42 +0100

    Introduce a wrapper for the UniTerm AST and merge syntaxes

--- src/parser/uniterm.rs
+++ src/parser/uniterm.rs
@@ -1,16 +1,151 @@
 //! Additional AST nodes for the common UniTerm syntax (see RFC002 for more details).
 use super::*;
-use crate::types::UnboundTypeVariableError;
 use error::ParseError;
 use utils::{build_record, FieldPathElem};
 
 use crate::{
     position::{RawSpan, TermPos},
     term::{MergePriority, MetaValue, RecordAttrs, RichTerm, Term},
-    types::{AbsType, Types},
+    types::{AbsType, Types, UnboundTypeVariableError},
 };
 
 use std::convert::TryFrom;
+
+/// A node of the uniterm AST. We only define new variants for constructs common to types and
+/// terms. Otherwise, we piggyback on the existing ASTs to avoid duplicating code and definitions.
+///
+/// During parsing, some constructs are common to both terms and types, such as variables or record
+/// literals ([`UniRecord`]s). They may be the source of ambiguities in the grammar, if we add two
+/// different derivation for them (e.g. one going first through term rules then types rules, and
+/// vice-versa).
+///
+/// To avoid the issue, we parse the source as a `UniTermNode`s, and give only one possible
+/// derivation - we will take a variable as an example in the following - through the parsing
+/// rules. As long as we can't decide yet how to see this variable, we keep it as a
+/// `UniTermNode::Var`.
+///
+/// As soon as this variable is used in a compound expression, the top-level rule tells us how to
+/// translate it. For example, if we see an arrow `a -> Num`, then we will convert it to a type
+/// variable, and return `UniTermNode::Types(AbsType::Arrow(..))`. If, on the other hand, we enter
+/// the rule for infix operator as in `a + 1`, `a` will be converted to a `Term::Var` and the
+/// resulting uniterm will be `UniTermNode::Term(Term::Op2(..))`.
+pub enum UniTermNode {
+    /// A variable. Can refer both to a term variable or a type variable.
+    Var(Ident),
+    /// A recod, that can be either a record literal or a record type.
+    Record(UniRecord),
+    /// A `UniTerm` that has been determined to be a term.
+    Term(RichTerm),
+    /// A `UniTerm` that has been determined to be a type.
+    Types(Types),
+}
+
+/// A uniterm with positional information.
+pub struct UniTerm {
+    node: UniTermNode,
+    pos: TermPos,
+}
+
+impl From<UniTermNode> for UniTerm {
+    fn from(node: UniTermNode) -> Self {
+        UniTerm {
+            node,
+            pos: TermPos::None,
+        }
+    }
+}
+
+impl UniTerm {
+    pub fn with_pos(mut self, pos: TermPos) -> Self {
+        self.pos = pos;
+        self
+    }
+}
+
+impl TryFrom<UniTerm> for Types {
+    type Error = ParseError;
+
+    fn try_from(ut: UniTerm) -> Result<Self, ParseError> {
+        match ut.node {
+            UniTermNode::Var(id) => Ok(Types(AbsType::Var(id))),
+            UniTermNode::Record(r) => Types::try_from(r),
+            UniTermNode::Types(ty) => Ok(ty),
+            UniTermNode::Term(rt) => Ok(Types(AbsType::Flat(rt))),
+            // => {
+            //     ty.contract().map_err(|UnboundTypeVariableError(id)| {
+            //         // We unwrap the position of the identifier, which must be set at this stage of parsing
+            //         let pos = id.pos;
+            //         ParseError::UnboundTypeVariables(vec![id], pos.unwrap())
+            //     })
+            // }
+        }
+    }
+}
+
+impl TryFrom<UniTerm> for RichTerm {
+    type Error = ParseError;
+
+    fn try_from(ut: UniTerm) -> Result<Self, ParseError> {
+        let UniTerm { node, pos } = ut;
+        let rt = match node {
+            UniTermNode::Var(id) => RichTerm::new(Term::Var(id), pos),
+            UniTermNode::Record(r) => RichTerm::try_from(r)?,
+            UniTermNode::Types(ty) => {
+                ty.contract().map_err(|UnboundTypeVariableError(id)| {
+                    // We unwrap the position of the identifier, which must be set at this stage of parsing
+                    let pos = id.pos;
+                    ParseError::UnboundTypeVariables(vec![id], pos.unwrap())
+                })?
+            }
+            UniTermNode::Term(rt) => rt,
+        };
+
+        Ok(rt.with_pos(pos))
+    }
+}
+
+impl From<RichTerm> for UniTerm {
+    fn from(rt: RichTerm) -> Self {
+        let pos = rt.pos;
+
+        UniTerm {
+            node: UniTermNode::Term(rt),
+            pos,
+        }
+    }
+}
+
+impl From<Term> for UniTerm {
+    fn from(t: Term) -> Self {
+        Self::from(RichTerm::from(t))
+    }
+}
+
+impl From<Types> for UniTerm {
+    fn from(ty: Types) -> Self {
+        UniTerm {
+            node: UniTermNode::Types(ty),
+            pos: TermPos::None,
+        }
+    }
+}
+
+impl From<UniTermNode> for UniTerm {
+    fn from(node: UniTermNode) -> Self {
+        UniTerm { node, pos }
+    }
+}
+
+impl From<UniRecord> for UniTerm {
+    fn from(ur: UniRecord) -> Self {
+        let pos = ur.pos;
+
+        UniTerm {
+            node: UniTermNode::Record(ur),
+            pos,
+        }
+    }
+}
 
 /// A record in the UniTerm syntax.
 #[derive(Clone)]
@@ -141,3 +276,26 @@ impl TryFrom<UniRecord> for Types {
         }
     }
 }
+
+// impl TryFrom<Types> for RichTerm {
+//     type Error = ParseError;
+//
+//     fn try_from(ty: Types) -> Result<Self, ParseError> {
+//         ty.contract().map_err(|UnboundTypeVariableError(id)| {
+//             // We unwrap the position of the identifier, which must be set at this stage of parsing
+//             let pos = id.pos;
+//             ParseError::UnboundTypeVariables(vec![id], pos.unwrap())
+//         })
+//     }
+// }
+//
+// // This `TryFrom` implementation can't actually fail. However, in order to have the macros
+// // `AsTypes`/`AsTerm` of the grammar to work uniformly, we have to require the same
+// // `TryFrom<A, Error = ParseError>` implementation for `UniRecord`, `Term`, and `Types`.
+// impl TryFrom<RichTerm> for Types {
+//     type Error = ParseError;
+//
+//     fn try_from(rt: RichTerm) -> Result<Self, ParseError> {
+//         Ok(Types(AbsType::Flat(rt)))
+//     }
+// }
