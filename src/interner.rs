7324bfd31abb8ca7af08a5d61857cedf6ffcaca9
Mahmoud Mazouz <mazouz.mahmoud@outlook.com>
2022-08-01 15:57:20 +0200

    Add necessary trait implementations for Symbol

--- src/interner.rs
+++ src/interner.rs
@@ -1,18 +1,24 @@
 //! Nickel string interner.
 
 use std::collections::HashMap;
-use std::sync::Arc;
+use std::rc::Rc;
+use std::{fmt, hash::Hash};
 
 #[derive(Debug, Clone)]
 /// A handle to an interned string.
-pub struct Symbol(Arc<str>);
+pub struct Symbol(Rc<str>);
 
 impl Symbol {
     /// Resolve an interned `Symbol`.
-    /// The interner isn't required and so its state needn't 
+    /// The interner isn't required and so its state needn't
     /// be passed around the evaluator.
-    pub fn resolve(&self) -> &str {
+    pub fn as_str(&self) -> &str {
         &*self.0
+    }
+
+    /// Create a Symbol from a String.
+    pub fn new(string: String) -> Self {
+        Self(Rc::from(string))
     }
 }
 
@@ -20,7 +26,35 @@ impl Symbol {
 // If we use that, cheap comparison on `Symbol` would fly out the window.
 impl PartialEq for Symbol {
     fn eq(&self, other: &Self) -> bool {
-        Arc::ptr_eq(&self.0, &other.0)
+        Rc::ptr_eq(&self.0, &other.0)
+    }
+}
+
+// TODO: document why this is reasonable.
+// See: https://doc.rust-lang.org/stable/std/hash/trait.Hash.html#hash-and-eq
+impl Eq for Symbol {}
+
+impl PartialOrd for Symbol {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.as_str().partial_cmp(other.as_str())
+    }
+}
+
+impl Ord for Symbol {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.as_str().cmp(other.as_str())
+    }
+}
+
+impl Hash for Symbol {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        std::ptr::hash(&*self.0, state);
+    }
+}
+
+impl fmt::Display for Symbol {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str(self.as_str())
     }
 }
 
@@ -35,24 +69,24 @@ impl Interner {
     /// Create an interner.
     pub fn new() -> Self {
         Self {
-            map: HashMap::new()
+            map: HashMap::new(),
         }
     }
 
     /// Create an interner with initial capacity.
     pub fn with_capacity(cap: usize) -> Self {
         Self {
-            map: HashMap::with_capacity(cap)
+            map: HashMap::with_capacity(cap),
         }
     }
 
-    /// Intern a string, or get the existing `Symbol` 
+    /// Intern a string, or get the existing `Symbol`
     // if the string happened to be interned before.
     pub fn intern(&mut self, string: &str) -> Symbol {
         match self.map.get(string) {
             Some(sym) => sym.clone(),
             None => {
-                let sym = Symbol(Arc::from(string));
+                let sym = Symbol(Rc::from(string));
                 self.map.insert(Box::from(string), sym.clone());
                 sym
             }
