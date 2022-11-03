a1057b6a9e64c54e06fb7d937cb5d7fbdd8c5204
francois-caddet <francois.caddet@icloud.com>
2022-11-03 17:46:08 +0100

    fix string concat for Nix

--- src/stdlib.rs
+++ src/stdlib.rs
@@ -77,4 +77,11 @@ pub mod compat {
             Term::Array(array, Default::default())
         )
     }
+
+    pub fn add() -> RichTerm {
+        op1(
+            UnaryOp::StaticAccess("add".into()),
+            Term::Var("compat".into()),
+        )
+    }
 }
