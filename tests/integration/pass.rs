b10b27fe616fcabdf9c9c90b22e9f821009ae40e
matthew healy <matthew-healy@users.noreply.github.com>
2022-12-15 12:06:10 +0100

    Automate adding `.ncl` file test cases (#992)
    * Add test-generator dependency
    
    * Use test-generator to simplify pass test file
    
    This commit uses the test-generator library to automatically generate a
    set of integration tests from any `.ncl` file in the
    `tests/integration/pass` directory.
    
    As a consequence, simply adding a new file to that directory is
    sufficient to add a new test case.
    
    This also meant that the non-test files, like the assertion library and
    files that are used to test importing, were moved to a '/lib' directory
    so test-generator doesn't pick them up.

--- tests/integration/pass.rs
+++ tests/integration/pass.rs
@@ -3,6 +3,7 @@ use nickel_lang_utilities::TestProgram;
 use std::ffi::OsString;
 use std::path::PathBuf;
 use std::thread;
+use test_generator::test_resources;
 
 // By default, tests are run with 2MB stack size, which can overflow in debug mode. We run the
 // tests with an increased stack size.
@@ -19,9 +20,10 @@ fn run(path: impl Into<OsString>) {
     );
 }
 
+#[test_resources("tests/integration/pass/*.ncl")]
 fn check_file(file: &str) {
     let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    path.push(format!("tests/integration/pass/{}", file));
+    path.push(file);
 
     thread::Builder::new()
         .name(String::from(file))
@@ -30,110 +32,4 @@ fn check_file(file: &str) {
         .unwrap()
         .join()
         .unwrap();
-}
-
-#[test]
-fn basics() {
-    check_file("basics.ncl");
-}
-
-#[test]
-fn builtins() {
-    check_file("builtins.ncl");
-}
-
-#[test]
-fn complete() {
-    check_file("complete.ncl");
-}
-
-#[test]
-fn contracts() {
-    check_file("contracts.ncl");
-}
-
-#[test]
-fn eq() {
-    check_file("eq.ncl")
-}
-
-#[test]
-fn functions() {
-    check_file("functions.ncl");
-}
-
-#[test]
-fn arrays() {
-    check_file("arrays.ncl");
-}
-
-#[test]
-fn metavalues() {
-    check_file("metavalues.ncl");
-}
-
-#[test]
-fn records() {
-    check_file("records.ncl");
-}
-
-#[test]
-fn record_defs() {
-    check_file("record-defs.ncl");
-}
-
-#[test]
-fn strings() {
-    check_file("strings.ncl");
-}
-
-#[test]
-fn typechecking() {
-    check_file("typechecking.ncl");
-}
-
-#[test]
-fn types() {
-    check_file("types.ncl");
-}
-
-#[test]
-fn serialize() {
-    check_file("serialize.ncl");
-    check_file("serialize-package.ncl");
-}
-
-#[test]
-fn annot_parsing() {
-    check_file("annotations.ncl");
-}
-
-#[test]
-fn importing() {
-    check_file("import.ncl");
-}
-
-#[test]
-fn overriding() {
-    check_file("overriding.ncl");
-}
-
-#[test]
-fn recursive_let() {
-    check_file("recursive_let.ncl");
-}
-
-#[test]
-fn quote_in_indentifier() {
-    check_file("quote_in_identifier.ncl")
-}
-
-#[test]
-fn priorities() {
-    check_file("priorities.ncl")
-}
-
-#[test]
-fn multiple_overrides() {
-    check_file("multiple-overrides.ncl")
 }
