9b07d34182516a01d98db7b29fceebf0c2d03ed3
Oghenevwogaga Ebresafe <gaga.ebresafe@tweag.io>
2023-01-17 13:33:17 +0100

    Move `pos` and `id_span` out of pattern matching

--- src/parser/utils.rs
+++ src/parser/utils.rs
@@ -282,14 +282,17 @@ pub fn elaborate_field_path(
         // unwrap is safe here becuase the initial content has a position,
         // and we make sure we assign a position for the next field.
         let acc_span = acc.pos.unwrap();
+        let pos = match path_elem {
+            FieldPathElem::Ident(id) => id.pos,
+            FieldPathElem::Expr(ref expr) => expr.pos,
+        };
+        // unwrap is safe here because every id should have a non-`TermPos::None` position
+        let id_span = pos.unwrap();
+        // `RawSpan::fuse` only returns `None` when the two spans are in different files.
+        // A record field and its value *must* be in the same file, so this is safe.
+        let pos = TermPos::Original(RawSpan::fuse(id_span, acc_span).unwrap());
         match path_elem {
             FieldPathElem::Ident(id) => {
-                // unwrap is safe here because every id should have a non-`TermPos::None` position
-                let id_span = id.pos.unwrap();
-                // `RawSpan::fuse` only returns `None` when the two spans are in different files.
-                // A record field and its value *must* be in the same file, so this is safe.
-                let pos = TermPos::Original(RawSpan::fuse(id_span, acc_span).unwrap());
-
                 let mut fields = HashMap::new();
                 fields.insert(id, acc);
 
@@ -310,10 +313,6 @@ pub fn elaborate_field_path(
                     }
                     _ => None,
                 };
-
-                let id_span = exp.pos.unwrap();
-                let pos = TermPos::Original(RawSpan::fuse(id_span, acc_span).unwrap());
-
                 if let Some(static_access) = static_access {
                     let id = Ident::new_with_pos(static_access, exp.pos);
                     let mut fields = HashMap::new();
