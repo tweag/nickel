26346bc9d6486c3db8e001df765c3094e7360e8e
francois-caddet <francois.caddet@icloud.com>
2023-01-24 17:10:56 +0100

    tests for Nix `with` inside a function body.

--- tests/nix/with.nix
+++ tests/nix/with.nix
@@ -41,12 +41,26 @@ in
   )
 
   # with and rec records
-  # TODO: this test is disabled because, now, when we are in a rec record, we don't populate the environment with it's fields. See how it's done for `let in` blocks.
+  # TODO: this test is disabled because, now, when we are in a rec record, we don't populate the environment with it's fields. See how it's done for `let in` blocks in `src/nix.rs`.
   #  (
   #    with r1; rec {
   #      a = 11;
   #      v =  a + 11;
   #      s = b + c;
   #    } == { a = 11; v = 22; s = "b1c1";}
+  #  )
+
+  # with inside a function body
+  # TODO: this test is disabled because, now, when we are in a function body, we don't populate the environment with it's fields. See how it's done for `let in` blocks in `src/nix.rs`.
+  #  (
+  #    let
+  #      f = a: with r1; [a b];
+  #    in f 11 == [11 r1.b]
+  #  )
+  # Don't forget to manage the patern matched arguments.
+  #  (
+  #    let
+  #      f = {a, b}: with r1; [a b c];
+  #    in f r2 == [r2.a r2.b r1.c]
   #  )
 ]
