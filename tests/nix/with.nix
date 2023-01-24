05c88b0cf325912fae7ac5d4ff64e2a8e6cfab0f
francois-caddet <francois.caddet@icloud.com>
2023-01-24 16:43:20 +0100

    more tests for Nix `with`
    - complex shadowing
    - nested record combined with a with
    - with and recursive record which does not pass for now  (check todos in
      `tests/nix/with.nix`)

--- tests/nix/with.nix
+++ tests/nix/with.nix
@@ -1,6 +1,7 @@
 let
   r1 = { a = "a1"; b = "b1"; c = "c1"; };
   r2 = { a = "a2"; b = "b2"; };
+  r3 = { a = { a = "raa"; }; };
 in
 [
   # simple with
@@ -24,4 +25,28 @@ in
     in
     with r1; with r2; [ a b c ] == [ 11 r2.b r1.c ]
   )
+  (
+    let
+      a = 11;
+    in
+    with r1; let
+      b = 22;
+    in
+    [ a b c ] == [ 11 22 r1.c ]
+  )
+
+  # with and nested records
+  (
+    with r3; a == r3.a && (with a; a == r3.a.a)
+  )
+
+  # with and rec records
+  # TODO: this test is disabled because, now, when we are in a rec record, we don't populate the environment with it's fields. See how it's done for `let in` blocks.
+  #  (
+  #    with r1; rec {
+  #      a = 11;
+  #      v =  a + 11;
+  #      s = b + c;
+  #    } == { a = 11; v = 22; s = "b1c1";}
+  #  )
 ]
