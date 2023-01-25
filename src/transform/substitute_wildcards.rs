28b55fa971a7528b2c7066a4d9e44b852a94a7cb
Yann Hamdaoui <yann.hamdaoui@tweag.io>
2023-01-25 22:03:29 +0100

    [REVIEW] Substitute wildcards in fields metadata
    Missing substitution of wildcards inside new FieldMetadata.

--- src/transform/substitute_wildcards.rs
+++ src/transform/substitute_wildcards.rs
@@ -9,7 +9,10 @@ use std::{convert::Infallible, rc::Rc};
 use crate::{
     label::Label,
     match_sharedterm,
-    term::{LabeledType, RichTerm, Term, Traverse, TraverseOrder, TypeAnnotation},
+    term::{
+        record::{Field, FieldMetadata, RecordData},
+        LabeledType, RichTerm, Term, Traverse, TraverseOrder, TypeAnnotation,
+    },
     typecheck::Wildcards,
     types::{TypeF, Types},
 };
@@ -27,49 +30,112 @@ pub fn transform_one(rt: RichTerm, wildcards: &Wildcards) -> RichTerm {
                 },
                 inner
             ) => {
-                // the followign line is due to a limitation of the macro `match_sharedterm`: we
-                // can't use `mut` directly inside patterns. See the associated documentation.
-                let mut annot = annot;
-                let LabeledType { types, label } = annot.types.take().unwrap();  // Safe, we know there's a type
+                RichTerm::new(Term::Annotated(annot.subst_wildcards(wildcards), inner), pos)
+            },
+            Term::RecRecord(record_data, dyn_fields, deps) => {
+                let record_data = record_data.subst_wildcards(wildcards);
+                let dyn_fields = dyn_fields
+                    .into_iter()
+                    .map(|(id_t, field)| (id_t, field.subst_wildcards(wildcards)))
+                    .collect();
 
-                // Replace the type annotation and the blame label
-                let types = substitute_wildcards_recursively(types, wildcards);
-                let label_types = substitute_wildcards_recursively(label.types.as_ref().clone(), wildcards);
-                let label = Label {
-                    types: Rc::new(label_types),
-                    ..label
-                };
-
-                annot.types = Some(LabeledType { types, label });
-
-                // TODO: should we also replace wildcards for contract annotation? It may be useful
-                // to use them, for example by annotationg a subexpression inside a typed block
-                // `exp | _`, saying "whatever the typechecker thinks the type of this expression should
-                // be, assume that it is".
-
-                RichTerm::new(Term::Annotated(annot, inner), pos)
+                RichTerm::new(Term::RecRecord(record_data, dyn_fields, deps), pos)
+            },
+            Term::Record(record_data) => {
+                RichTerm::new(
+                    Term::Record(record_data.subst_wildcards(wildcards)),
+                    pos,
+                )
             },
         } else rt
     }
 }
 
-/// Get the inferred type for a wildcard, or Dyn if no type was inferred.
+/// Get the inferred type for a wildcard, or `Dyn` if no type was inferred.
 fn get_wildcard_type(wildcards: &Wildcards, id: usize) -> Types {
     wildcards.get(id).cloned().unwrap_or(Types(TypeF::Dyn))
 }
 
-/// Recursively substitutes wildcards for their inferred type inside a given type.
-fn substitute_wildcards_recursively(ty: Types, wildcards: &Wildcards) -> Types {
-    ty.traverse::<_, _, Infallible>(
-        &|ty, _| {
-            if let Types(TypeF::Wildcard(id)) = ty {
-                Ok(get_wildcard_type(wildcards, id))
-            } else {
-                Ok(ty)
-            }
-        },
-        &mut (),
-        TraverseOrder::TopDown,
-    )
-    .unwrap()
+trait SubstWildcard {
+    /// Recursively substitutes wildcards for their inferred type inside a given type.
+    fn subst_wildcards(self, wildcards: &Wildcards) -> Self;
+}
+
+impl SubstWildcard for Types {
+    fn subst_wildcards(self, wildcards: &Wildcards) -> Types {
+        self.traverse::<_, _, Infallible>(
+            &|ty, _| {
+                if let Types(TypeF::Wildcard(id)) = ty {
+                    Ok(get_wildcard_type(wildcards, id))
+                } else {
+                    Ok(ty)
+                }
+            },
+            &mut (),
+            TraverseOrder::TopDown,
+        )
+        .unwrap()
+    }
+}
+
+impl SubstWildcard for LabeledType {
+    fn subst_wildcards(self, wildcards: &Wildcards) -> LabeledType {
+        LabeledType {
+            types: self.types.subst_wildcards(wildcards),
+            label: self.label.subst_wildcards(wildcards),
+        }
+    }
+}
+
+impl SubstWildcard for Label {
+    fn subst_wildcards(self, wildcards: &Wildcards) -> Label {
+        Label {
+            types: Rc::new((&*self.types).clone().subst_wildcards(wildcards)),
+            ..self
+        }
+    }
+}
+
+impl SubstWildcard for TypeAnnotation {
+    fn subst_wildcards(self, wildcards: &Wildcards) -> TypeAnnotation {
+        let types = self
+            .types
+            .map(|labeled_ty| labeled_ty.subst_wildcards(wildcards));
+        let contracts = self
+            .contracts
+            .into_iter()
+            .map(|labeled_ty| labeled_ty.subst_wildcards(wildcards))
+            .collect();
+
+        TypeAnnotation { types, contracts }
+    }
+}
+
+impl SubstWildcard for FieldMetadata {
+    fn subst_wildcards(self, wildcards: &Wildcards) -> FieldMetadata {
+        FieldMetadata {
+            annotation: self.annotation.subst_wildcards(wildcards),
+            ..self
+        }
+    }
+}
+
+impl SubstWildcard for Field {
+    fn subst_wildcards(self, wildcards: &Wildcards) -> Field {
+        let metadata = self.metadata.subst_wildcards(wildcards);
+        Field { metadata, ..self }
+    }
+}
+
+impl SubstWildcard for RecordData {
+    fn subst_wildcards(self, wildcards: &Wildcards) -> RecordData {
+        RecordData {
+            fields: self
+                .fields
+                .into_iter()
+                .map(|(id, field)| (id, field.subst_wildcards(wildcards)))
+                .collect(),
+            ..self
+        }
+    }
 }
