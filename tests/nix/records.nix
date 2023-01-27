eb4a9598835a6c2eae9096846780aa6ef0dcc212
francois-caddet <francois.caddet@icloud.com>
2023-01-27 17:13:05 +0100

    add the `or <default>` form for a field access in Nix

--- tests/nix/records.nix
+++ tests/nix/records.nix
@@ -9,4 +9,11 @@
   ({ a.foo = 1; } ? "a.foo" == false)
   ({ a.foo = 1; } ? "a".foo == true)
   ({ a.foo = 1; } ? a == true)
+
+  # field access or default
+  ({ a = "a"; }.a or "x" == "a")
+  ({ a = "a"; }.b or "x" == "x")
+  ({ a.b = "ab"; }.a or "x" == { b = "ab"; })
+  ({ a.b = "ab"; }.a.b or "x" == "ab")
+  ({ a.b = "ab"; }.a.c or "x" == "x")
 ]
