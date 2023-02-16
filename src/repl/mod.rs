fb3a924f3d2b8d4922b1a385a3987472f80a11b4
Oghenevwogaga Ebresafe <gaga.ebresafe@tweag.io>
2023-02-16 15:24:02 +0100

    [FORMAT]

--- src/repl/mod.rs
+++ src/repl/mod.rs
@@ -242,7 +242,10 @@ impl<EC: EvalCache> Repl for ReplImpl<EC> {
         let (term, pending) =
             import_resolution::resolve_imports(term, self.vm.import_resolver_mut())?;
         for id in &pending {
-            self.vm.import_resolver_mut().resolve_imports(*id, false).unwrap();
+            self.vm
+                .import_resolver_mut()
+                .resolve_imports(*id, false)
+                .unwrap();
         }
         typecheck::env_add_term(
             &mut self.env.type_ctxt.type_env,
@@ -266,7 +269,10 @@ impl<EC: EvalCache> Repl for ReplImpl<EC> {
         let (term, pending) =
             import_resolution::resolve_imports(term, self.vm.import_resolver_mut())?;
         for id in &pending {
-            self.vm.import_resolver_mut().resolve_imports(*id, false).unwrap();
+            self.vm
+                .import_resolver_mut()
+                .resolve_imports(*id, false)
+                .unwrap();
         }
         let wildcards =
             typecheck::type_check(&term, self.env.type_ctxt.clone(), self.vm.import_resolver())?;
