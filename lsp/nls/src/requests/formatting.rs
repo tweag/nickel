4fcb51de46a9bf2c5422eaa89bc1c02e70ee86cf
Oghenevwogaga Ebresafe <gaga.ebresafe@tweag.io>
2023-03-31 15:55:40 +0100

    Some structure to the formatting module

--- lsp/nls/src/requests/formatting.rs
+++ lsp/nls/src/requests/formatting.rs
@@ -1,5 +1,5 @@
-use lsp_server::{RequestId, ResponseError};
-use lsp_types::DocumentFormattingParams;
+use lsp_server::{RequestId, Response, ResponseError};
+use lsp_types::{DocumentFormattingParams, Position, Range, TextEdit};
 
 use crate::server::Server;
 
@@ -8,5 +8,26 @@ pub fn handle_format_document(
     id: RequestId,
     server: &mut Server,
 ) -> Result<(), ResponseError> {
+    let document_id = params.text_document.uri.to_file_path().unwrap();
+    let file_id = server.cache.id_of(document_id).unwrap();
+    let text = server.cache.files().source(file_id);
+
+    let new_text = String::new();
+    let result = Some(vec![TextEdit {
+        range: Range {
+            start: Position {
+                line: 0,
+                character: 0,
+            },
+            // for now
+            end: Position {
+                line: 0,
+                character: 0,
+            },
+        },
+        new_text,
+    }]);
+    let response = Response::new_ok(id, result);
+
     Ok(())
 }
