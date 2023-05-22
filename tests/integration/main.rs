6344e93c35debd4706faea1d6ba909cc1d321bc1
matthew healy <matthew-healy@users.noreply.github.com>
2023-05-22 02:12:58 +0200

    Convert tests to new annotated format, pt. 1 (#1301)
    * Use single quote for consistency
    
    * Convert contracts_fail to ncl files
    
    * Convert existing ncl source files to annotated tests
    
    This commit converts every existing .ncl file used in tests to an
    annotated source file. This means we can change the glob we use to
    pick up every .ncl file in the integration test subdirectory, which
    makes test organisation significantly more flexible (and should
    solve the problem of very long test file names by supporting arbitrary
    nesting).
    
    One additional change I had to make here, which increased the diff
    significantly, is to add `repeat` functionality to the test annotations,
    which allow a test to be re-run an arbitrary number of times. This is
    to support tests where behaviour may be non-deterministic (e.g. it
    occurs as the result of iterating through a `HashMap`).

--- tests/integration/main.rs
+++ tests/integration/main.rs
@@ -1,7 +1,7 @@
 use std::{io::Cursor, thread};
 
 use nickel_lang::{
-    error::{Error, EvalError},
+    error::{Error, EvalError, TypecheckError},
     term::Term,
 };
 use nickel_lang_utilities::{
@@ -11,14 +11,12 @@ use nickel_lang_utilities::{
 use serde::Deserialize;
 use test_generator::test_resources;
 
-mod contracts_fail;
-mod destructuring;
+mod contract_label_path;
 mod free_vars;
 mod imports;
 mod infinite_rec;
 mod merge_fail;
 mod parse_fail;
-mod pass;
 mod pretty;
 mod query;
 mod records_fail;
@@ -27,9 +25,9 @@ mod stdlib_typecheck;
 mod typecheck_fail;
 mod unbound_type_variables;
 
-#[test_resources("./tests/integration/fail/*.ncl")]
+#[test_resources("./tests/integration/**/*.ncl")]
 fn check_annotated_nickel_file(path: &str) {
-    let test: TestCase<Expectation> =
+    let test: TestCase<Test> =
         read_annotated_test_case(path).expect("Failed to parse annotated program");
 
     // By default, cargo runs tests with a 2MB stack, which we can overflow in
@@ -46,31 +44,47 @@ fn check_annotated_nickel_file(path: &str) {
         .expect("Failed to join thread")
 }
 
-fn run_test(test: TestCase<Expectation>, path: String) {
-    let mut p = TestProgram::new_from_source(Cursor::new(test.program), path.as_str()).expect("");
-    let result = p.eval();
-    match test.annotation {
-        Expectation::Error(expected_err) => {
-            let err = result.expect_err(
-                format!(
-                    "Expected error: {}, but program evaluated successfully.",
-                    expected_err
+fn run_test(test_case: TestCase<Test>, path: String) {
+    let repeat = test_case.annotation.repeat.unwrap_or(1);
+    let program = test_case.program;
+    let test = test_case.annotation.test;
+
+    for _ in 0..repeat {
+        let mut p =
+            TestProgram::new_from_source(Cursor::new(program.clone()), path.as_str()).expect("");
+        match test.clone() {
+            Expectation::Error(expected_err) => {
+                let result = p.eval();
+                let err = result.expect_err(
+                    format!(
+                        "Expected error: {}, but program evaluated successfully.",
+                        expected_err
+                    )
+                    .as_str(),
+                );
+                assert_eq!(expected_err, err, "wrong error evaluating file {path}")
+            }
+            Expectation::Pass => {
+                let result = p.eval();
+                assert_eq!(
+                    result.map(Term::from),
+                    Ok(Term::Bool(true)),
+                    "unexpected error evaluating file {path}",
                 )
-                .as_str(),
-            );
-            assert_eq!(expected_err, err, "wrong error evaluating file {path}")
+            }
+            Expectation::Skip => (),
         }
-        Expectation::Pass => assert_eq!(
-            result.map(Term::from),
-            Ok(Term::Bool(true)),
-            "unexpected error evaluating file {path}",
-        ),
-        Expectation::Skip => (),
     }
 }
 
 #[derive(Deserialize)]
-#[serde(tag = "test", content = "metadata")]
+struct Test {
+    test: Expectation,
+    repeat: Option<usize>,
+}
+
+#[derive(Clone, Deserialize)]
+#[serde(tag = "type", content = "metadata")]
 enum Expectation {
     #[serde(rename = "error")]
     Error(ErrorExpectation),
@@ -80,7 +94,7 @@ enum Expectation {
     Skip,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Clone, Deserialize)]
 #[serde(tag = "error", content = "expectation")]
 #[allow(clippy::enum_variant_names)]
 enum ErrorExpectation {
@@ -93,8 +107,20 @@ enum ErrorExpectation {
     EvalNAryPrimopTypeError,
     #[serde(rename = "EvalError::BlameError")]
     EvalBlameError,
+    #[serde(rename = "EvalError::IllegalPolymorphicTailAccess")]
+    EvalIllegalPolymorphicTailAccess,
     #[serde(rename = "EvalError::TypeError")]
     EvalTypeError,
+    #[serde(rename = "EvalError::FieldMissing")]
+    EvalFieldMissing { field: String },
+    #[serde(rename = "TypecheckError::UnboundIdentifier")]
+    TypecheckUnboundIdentifier { identifier: String },
+    #[serde(rename = "TypecheckError::TypeMismatch")]
+    TypecheckTypeMismatch { expected: String, found: String },
+    #[serde(rename = "TypecheckError::MissingRow")]
+    TypecheckMissingRow { ident: String },
+    #[serde(rename = "TypecheckError::RowMismatch")]
+    TypecheckRowMismatch,
 }
 
 impl PartialEq<Error> for ErrorExpectation {
@@ -102,10 +128,34 @@ impl PartialEq<Error> for ErrorExpectation {
         use ErrorExpectation::*;
         match (self, other) {
             (EvalBlameError, Error::EvalError(EvalError::BlameError { .. }))
+            | (
+                EvalIllegalPolymorphicTailAccess,
+                Error::EvalError(EvalError::IllegalPolymorphicTailAccess { .. }),
+            )
             | (EvalTypeError, Error::EvalError(EvalError::TypeError(..)))
             | (EvalEqError, Error::EvalError(EvalError::EqError { .. }))
             | (EvalNAryPrimopTypeError, Error::EvalError(EvalError::NAryPrimopTypeError { .. }))
-            | (EvalOther, Error::EvalError(EvalError::Other(..))) => true,
+            | (EvalOther, Error::EvalError(EvalError::Other(..)))
+            | (TypecheckRowMismatch, Error::TypecheckError(TypecheckError::RowMismatch(..))) => {
+                true
+            }
+            (EvalFieldMissing { field }, Error::EvalError(EvalError::FieldMissing(ident, ..)))
+                if field == ident =>
+            {
+                true
+            }
+            (
+                TypecheckUnboundIdentifier { identifier },
+                Error::TypecheckError(TypecheckError::UnboundIdentifier(ident, ..)),
+            ) if ident.label() == identifier => true,
+            (
+                TypecheckTypeMismatch { expected, found },
+                Error::TypecheckError(TypecheckError::TypeMismatch(expected1, found1, ..)),
+            ) if expected == &expected1.to_string() && found == &found1.to_string() => true,
+            (
+                TypecheckMissingRow { ident },
+                Error::TypecheckError(TypecheckError::MissingRow(row, ..)),
+            ) if ident == row.label() => true,
             (_, _) => false,
         }
     }
@@ -115,12 +165,34 @@ impl std::fmt::Display for ErrorExpectation {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         use ErrorExpectation::*;
         let name = match self {
-            EvalBlameError => "EvalError::BlameError",
-            EvalTypeError => "EvalError::TypeError",
-            EvalEqError => "EvalError::EqError",
-            EvalOther => "EvalError::Other",
-            EvalNAryPrimopTypeError => "EvalError::NAryPrimopTypeError",
+            EvalBlameError => "EvalError::BlameError".to_owned(),
+            EvalTypeError => "EvalError::TypeError".to_owned(),
+            EvalEqError => "EvalError::EqError".to_owned(),
+            EvalOther => "EvalError::Other".to_owned(),
+            EvalNAryPrimopTypeError => "EvalError::NAryPrimopTypeError".to_owned(),
+            EvalIllegalPolymorphicTailAccess => {
+                "EvalError::IllegalPolymorphicTailAccess".to_owned()
+            }
+            EvalFieldMissing { field } => {
+                format!("EvalError::FieldMissing({field})")
+            }
+            TypecheckUnboundIdentifier { identifier } => {
+                format!("TypecheckError::UnboundIdentifier({identifier})")
+            }
+            TypecheckTypeMismatch { expected, found } => {
+                format!("TypecheckError::TypeMismatch({expected}, {found})")
+            }
+            TypecheckMissingRow { ident } => {
+                format!("TypecheckError::MissingRow({ident})")
+            }
+            TypecheckRowMismatch => "TypecheckError::RowMismatch".to_owned(),
         };
         write!(f, "{}", name)
+    }
+}
+
+impl std::fmt::Debug for ErrorExpectation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self)
     }
 }
