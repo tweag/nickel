310ce9650e343c24315fefb81aa8f3eae836ba6d
Viktor Kleen <viktor.kleen@tweag.io>
2023-06-28 14:38:38 +0000

    Stop recompiling the quoting regex for printing identifiers (#1412)
    As observed by @jneem we were constantly recompiling the regular
    expression determining whether quoting an identifier is necessary.
    This change improves the pretty printer performance on a non-synthetic
    example by a factor of 3.

--- core/src/identifier.rs
+++ core/src/identifier.rs
@@ -11,6 +11,7 @@ use crate::{
 
 simple_counter::generate_counter!(GeneratedCounter, usize);
 static INTERNER: Lazy<interner::Interner> = Lazy::new(interner::Interner::new);
+static QUOTING_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new("^_?[a-zA-Z][_a-zA-Z0-9-]*$").unwrap());
 
 #[derive(Clone, Copy, Deserialize, Serialize)]
 #[serde(into = "String", from = "String")]
@@ -58,9 +59,8 @@ impl Ident {
     /// label isn't a valid identifier according to the parser, for example if it contains a
     /// special character like a space.
     pub fn label_quoted(&self) -> String {
-        let reg = Regex::new("^_?[a-zA-Z][_a-zA-Z0-9-]*$").unwrap();
         let label = self.label();
-        if reg.is_match(label) && !KEYWORDS.contains(&label) {
+        if QUOTING_REGEX.is_match(label) && !KEYWORDS.contains(&label) {
             String::from(label)
         } else {
             format!("\"{}\"", escape(label))
