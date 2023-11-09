0d85f1e438fffee020fbdf2e0576fcca912eec8e
Yann Hamdaoui <yann.hamdaoui@tweag.io>
2023-11-09 16:47:23 +0100

    Remove debug println and fix code comment

--- core/src/error/suggest.rs
+++ core/src/error/suggest.rs
@@ -7,10 +7,10 @@
 
 use strsim::normalized_damerau_levenshtein;
 
-/// The maximum distance between two strings for them to be considered similar and suggested to the
-/// other. The current threshold is rather low, because short words with edit distance 1 such as
-/// `bar` and `bare` might have a similarity which isn't so high.
-pub const MIN_SIMILARITY: f64 = 0.50;
+/// The minimum similarity between the user's input and an existing symbol for the symbol to be
+/// considered a serious candidate. The current threshold is rather low, because short words with
+/// edit distance 1 such as `bar` and `bare` might have a similarity which isn't that high.
+pub const MIN_SIMILARITY: f64 = 0.60;
 
 /// Find the closest match to the user input in the given list of symbols, to be used as a
 /// suggestion. If there is an exact match (modulo casing), it is returned immediately. Otherwise,
@@ -28,7 +28,6 @@ where
     let mut arg_max: Option<&'syms str> = None;
 
     if symbols.is_empty() {
-        println!("Empty list of symbols");
         return None;
     }
 
@@ -40,9 +39,7 @@ where
 
         let similarity = normalized_damerau_levenshtein(sym.as_ref(), user_input.as_ref());
 
-        println!("Found similary {similarity} for {}", sym.as_ref());
-
-        if similarity > max && similarity > MIN_SIMILARITY {
+        if similarity > max && similarity >= MIN_SIMILARITY {
             arg_max = Some(sym.as_ref());
             max = similarity;
         }
