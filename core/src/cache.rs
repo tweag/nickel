7fe5946a61cca8135ffa3b6a8a3ec25afbfc77e3
Quantum64 <Quantum64@users.noreply.github.com>
2023-12-05 03:43:27 -0500

    Support importing txt files as strings (#1734)
    * Support importing txt files as strings
    
    * Change String to Raw

--- core/src/cache.rs
+++ core/src/cache.rs
@@ -41,6 +41,7 @@ pub enum InputFormat {
     Toml,
     #[cfg(feature = "nix-experimental")]
     Nix,
+    Raw,
 }
 
 impl InputFormat {
@@ -53,6 +54,7 @@ impl InputFormat {
             Some("toml") => Some(InputFormat::Toml),
             #[cfg(feature = "nix-experimental")]
             Some("nix") => Some(InputFormat::Nix),
+            Some("txt") => Some(InputFormat::Raw),
             _ => None,
         }
     }
@@ -616,6 +618,10 @@ impl Cache {
                     .map(|t| (attach_pos(t), ParseErrors::default()))
                     .map_err(|err| ParseError::from_serde_json(err, file_id, &self.files))
             }
+            InputFormat::Raw => Ok((
+                attach_pos(Term::Str(self.files.source(file_id).into()).into()),
+                ParseErrors::default(),
+            )),
         }
     }
 
