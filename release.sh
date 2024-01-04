54e3436f7e71e9f0bb5e4fb2d1b08f251dfc1f14
Yann Hamdaoui <yann.hamdaoui@tweag.io>
2024-01-04 10:38:58 +0100

    Change casing, use namerefs and other small improvements

--- release.sh
+++ release.sh
@@ -1,45 +1,32 @@
 #!/usr/bin/env bash
 
+# Perform clean up actions upon unexpected exit.
 fn cleanup() {
   echo "++ Unexpected exit. Cleaning up..."
   set +e
 
-  for ((i=${#cleanupActions[@]}-1; i>=0; i--)); do
-    ${cleanupActions[$i]} || true
+  for ((i=${#cleanup_actions[@]}-1; i>=0; i--)); do
+    ${cleanup_actions[$i]} || true
   done
 
-  cleanupActions=()
-
+  cleanup_actions=()
   exit 1
 }
-
-trap cleanup ERR
-set -eEuo pipefail
-
-if [[ "$1" != "major" && "$1" != "minor" && "$1" != "patch" ]]; then
-    echo "Invalid argument: $1" >&2
-    echo "Usage: $0 <major|minor|patch>" >&2
-    exit 1
-fi
-
-# A stack of actions to perform upon unexpected exit. Cleanup actions are are
-# indeed popped from the top of cleanupActions (that is, in reverse order, when
-# seen as an array)
-cleanupActions=()
 
 # Take a subdirectory containing a crate of the current workspace as the first
 # argument and a variable name as the second argument. Read the crate version
 # from Cargo.toml and populate the variable with an array of the three version
 # numbers (major, minor, patch).
-fn readCrateVersion() {
-  local -n versionArray=$2 # use nameref for indirection: this will populate the variable named by the first argument
+fn read_crate_version() {
+  local -n version_array=$2 # use nameref for indirection: this will populate the variable named by the first argument
   local version=$(tomlq -r .package.version "$1/Cargo.toml")
-  readarray -td'.' versionArray <<< "$version"
+  readarray -td'.' version_array <<< "$version"
 }
 
-# Take a string message as an argument and ask the user to confirm the action.
+# Take a string message as an argument and ask the user to confirm that the
+# script should proceed with the next actions.
 # If the user doesn't confirm, exit the script.
-fn confirmAction() {
+fn confirm_proceed() {
   read -p "$1. Proceed (y/n)?" -n 1 -r
 
   if [[ $REPLY =~ ^[Nn]$ ]]; then
@@ -51,77 +38,136 @@ fn confirmAction() {
 
 # Take a bash array representing a version (major, minor, patch) as an argument
 # and print the corresponding version string ("major.minor.patch")
-fn printVersionArray() {
-    local version=("$@")
+# This function uses nameref, so the argument must be a variable name and not a
+# value.
+#
+# For example:
+# ```
+#   local version=(1 2 3)
+#   print_version_array version
+# ```
+fn print_version_array() {
+    local -n version=$1
     echo "${version[0]}.${version[1]}.${version[2]}"
 }
 
-# Go through a Cargo.toml file and bump all dependencies to local crates that are being updated (in practice, the crates populating versionMap)
+# Go through a Cargo.toml file and bump all dependencies to local crates that are being updated (in practice, the crates populating version_map)
 # Arguments:
 # - $1: the type of Cargo.toml, either "workspace" or "crate"
 # - $2: the path to the Cargo.toml file
-# - $3: the version map, a bash associative array mapping updated crate names to new versions
-fn bumpDependencies() {
-    local pathCargoToml="$2"
-    local versionMap="$3"
+# - $3: the version map, a bash associative array mapping updated crate names to
+#   new versions. This argument is taken by nameref, so you must pass the name
+#   of an existing variable containing the map, not a value. We do so because
+#   passing associative arrays by value is painful in bash.
+#
+# For example:
+# ```
+#   local -A version_map
+#   version_map["nickel-lang-core"]="1.2.3"
+#   update_dependencies "workspace" "./Cargo.toml" version_map
+# ```
+fn update_dependencies() {
+    local path_cargo_toml="$2"
+    local -n version_map=$3
 
     # If we are looking at a crate's Cargo.toml file, the dependencies are
     # located at .dependencies. If we are looking at the workspace's Cargo.toml,
     # the dependencies are located at .workspace.dependencies. This difference
-    # is abstracted away in `depsPath`
-    local depsPath
+    # is abstracted away in `dependencies_path`
+    local dependencies_path
 
     if [[ $1 == "workspace" ]]; then
-        depsPath=".workspace.dependencies"
+        dependencies_path=".workspace.dependencies"
+    elif [[ $1 == "crate" ]]; then
+        dependencies_path=".dependencies"
     else
-        depsPath=".dependencies"
+        echo "[Internal error] Invalid argument for update_dependencies(): expected \'crate\' or \'workspace\', got \'$1\'" >&2
+        exit 1
     fi
 
-    local dependencies=($(tomlq -r '('$depsPath' | keys[])' "$crate/Cargo.toml"))
+    local dependencies=($(tomlq -r '('$dependencies_path' | keys[])' "$crate/Cargo.toml"))
 
     for $dependency in dependencies; do
-        if [[ -v versionMap["$dependency"] ]]; then
-            local depType=$(tomlq -r '('$depsPath'."'$dependency'" | type)' "$crate/Cargo.toml")
-            local hasVersion=$(tomlq -r '('$depsPath'."'$dependency'" | has("version")' "$crate/Cargo.toml")
+        # If the dependency is in the version map, it means that we might have
+        # bumped it as part of the release process. In that case, we need to
+        # update the dependency to the new version.
+        if [[ -v version_map["$dependency"] ]]; then
+            # Cargo dependencies can be either specified as a simple version
+            # string, as in
+            # `foo_crate = "1.2.3"`
+            # or as an object with a `version` field, as in
+            # `foo_crate = { version = "1.2.3", features = ["bar"] }`
+            # The updated thus depend on the type of the dependency field, which
+            # can be determined by tomlq's `type` function
+            local dependency_type=$(tomlq -r '('$dependencies_path'."'$dependency'" | type)' "$crate/Cargo.toml")
+            local has_version=$(tomlq -r '('$dependencies_path'."'$dependency'" | has("version")' "$crate/Cargo.toml")
 
-            if [[ $depType == "string" ]]; then
-                echo "Patching cross-dependency $dependency in $crate to version ${versionMap[$dependency]}"
-                tomlq -i $depsPath'."'$dependency'" = "'${versionMap[$dependency]}'"' "$crate/Cargo.toml"
-            elif [[ $depType == "object" && $hasVersion == "true" ]]; then
-                echo "Patching cross-dependency $dependency in $crate to version ${versionMap[$dependency]}"
+            if [[ $dependency_type == "string" ]]; then
+                echo "Patching cross-dependency $dependency in $crate to version ${version_map[$dependency]}"
+                tomlq -i $dependencies_path'."'$dependency'" = "'${version_map[$dependency]}'"' "$crate/Cargo.toml"
+            # Most of local crates use the workspace's version by default, i.e.
+            # are of the form `foo_crate = { workspace = true }`. In that case,
+            # the update is already taken care of when updating the workspace's
+            # Cargo.toml, so we don't do anything if the dependency doesn't have
+            # a version field
+            elif [[ $dependency_type == "object" && $has_version == "true" ]]; then
+                echo "Patching cross-dependency $dependency in $crate to version ${version_map[$dependency]}"
                 # The dependency might be set to follow the workspace's version,
                 # in which case we don't touch it
 
-                tomlq -i $depsPath'."'$dependency'".version = "'${versionMap[$dependency]}'"' "$crate/Cargo.toml"
+                tomlq -i $dependencies_path'."'$dependency'".version = "'${version_map[$dependency]}'"' "$crate/Cargo.toml"
             fi
 
             git add "$crate/Cargo.toml"
-            cleanupActions+=("git restore \"$crate/Cargo.toml\"")
+            cleanup_actions+=("git restore \"$crate/Cargo.toml\"")
         fi
     done
 }
+
+fn print_usage_and_exit() {
+    echo "Usage: $0 <major|minor|patch>" >&2
+    exit 1
+}
+
+# Report progress to the user with adated indentation
+fn report_progress() {
+    echo "  -- $1"
+}
+
+trap cleanup ERR
+set -eEuo pipefail
+
+if [[ "$1" != "major" && "$1" != "minor" && "$1" != "patch" ]]; then
+    echo "Invalid argument: $1" >&2
+    print_usage_and_exit
+fi
+
+# A stack of actions to perform upon unexpected error. Cleanup actions are are
+# indeed popped from the top of cleanup_actions (that is, in reverse order, when
+# seen as an array)
+cleanup_actions=()
 
 echo <<EOF
 ++ Nickel release script
 ++
 ++ This script will:
 ++
-++ - Automatically bump version numbers in Cargo.toml files
-++ - Bump dependencies to local crates accordingly
+++ - Bump version numbers in Cargo.toml files
+++ - Bump local dependencies to local crates accordingly
 ++ - Commit and push the changes on a new release branch
-++ - Make the stable branch point to the release branch
+++ - Make the remote 'stable' branch point to the release branch
 ++ - Publish relevant crate to crates.io
 ++
-++ Sanity checks (build, test, publish dry run etc.) are performed along the
-++ way, and in case of failure, the release script will try to restore things to
-++ the previous state as much as possible
+++ Sanity checks (build, test, publish --dry-run etc.) are performed along the
+++ way. In case of failure, this release script will its best to restore things
+to ++ the previous state as much as possible
 EOF
 
-confirmAction ""
+confirm_proceed ""
 
 # Check that the working directory is clean
 if ! git status --untracked-files=no --porcelain; then
-    confirmAction "Working directory is not clean"
+    confirm_proceed "Working directory is not clean"
 fi
 
 echo "\n++ Prepare release branch creation"
@@ -129,55 +175,55 @@ echo "\n++ Prepare release branch creation"
 # Directories of subcrates following their own independent versioning
 local independentCrates=(core utils lsp/lsp-harness ./wasm-repl)
 # All subcrate directories, including the ones above
-local allCrates=("${independentCrates[@]}" cli lsp/nls pyckel)
+local all_crates=("${independentCrates[@]}" cli lsp/nls pyckel)
 
-local workspaceVersion=$(tomlq -r .workspace.package.version ./Cargo.toml)
-local workspaceVersionArray
-readarray -td'.' workspaceVersionArray <<< "$workspaceVersion"
+local workspace_version=$(tomlq -r .workspace.package.version ./Cargo.toml)
+local workspace_version_array
+readarray -td'.' workspace_version_array <<< "$workspace_version"
 
-local newWorkspaceVersion
+local new_workspace_version
 
 # We checked at the beginning of the script that $1 was either "major", "minor"
 # or "patch", so we don't need to handle cath-all case.
 if [[ $1 == "major" ]]; then
-    newWorkspaceVersion=$((workspaceVersionArray[0] + 1)).0.0
+    new_workspace_version=$((workspace_version_array[0] + 1)).0.0
 elif [[ $1 == "minor" ]]; then
-    newWorkspaceVersion=${workspaceVersionArray[0]}.$((workspaceVersionArray[1] + 1)).0
+    new_workspace_version=${workspace_version_array[0]}.$((workspace_version_array[1] + 1)).0
 elif [[ $1 == "patch" ]]; then
-    newWorkspaceVersion=${workspaceVersionArray[0]}.${workspaceVersionArray[1]}.$((workspaceVersionArray[2] + 1))
+    new_workspace_version=${workspace_version_array[0]}.${workspace_version_array[1]}.$((workspace_version_array[2] + 1))
 fi
 
-confirmAction "Updating to version $(printVersionArray "${newWorkspaceVersion[@]}")"
+confirm_proceed "Updating to version $new_workspace_version"
 
-echo "Creating release branch..."
+report_progress "Creating release branch..."
 
-local releaseBranch="$newWorkspaceVersion-release"
+local release_branch="$new_workspace_version-release"
 
-git switch --create $releaseBranch
-cleanupActions+=("git branch -d $releaseBranch; git switch master")
+git switch --create $release_branch
+cleanup_actions+=("git branch -d $release_branch; git switch master")
 
-echo "Bumping workspace version number..."
+report_progress "Bumping workspace version number..."
 
 tomlq -i '.workspace.package.version = "'$newVersion'"' ./Cargo.toml
 git add ./Cargo.toml
-cleanupActions+=("git restore ./Cargo.toml")
+cleanup_actions+=("git restore ./Cargo.toml")
 
-echo "Bumping other crates version numbers..."
+report_progress "Bumping other crates version numbers..."
 
-local crateVersionArray
-local newCrateVersion
+local crate_version_array
+local new_crate_version
 
 for $crate in $independentCrates; do
-    readCrateVersion "$crate" crateVersionArray
+    read_crate_version "$crate" crate_version_array
 
-    newCrateVersion=${crateVersionArray[0]}.$((crateVersionArray[1] + 1)).0
+    new_crate_version=${crate_version_array[0]}.$((crate_version_array[1] + 1)).0
 
-    read -p "$crate is currently in version $(printVersionArray "${crateVersionArray[@]}"). Bump to the next version $newCrateVersion [if no, you'll have a pause later to manually bump those versions if needed] (y/n) ?" -n 1 -r
+    read -p "$crate is currently in version $(print_version_array crate_version_array). Bump to the next version $new_crate_version [if no, you'll have a pause later to manually bump those versions if needed] (y/n) ?" -n 1 -r
 
     if [[ $REPLY =~ ^[Yy]$ ]]; then
       tomlq -i '.package.version = "'$newVersion'"' "$crate/Cargo.toml"
       git add "$crate/Cargo.toml"
-      cleanupActions+=("git restore \"$crate/Cargo.toml\"")
+      cleanup_actions+=("git restore \"$crate/Cargo.toml\"")
     fi
 done
 
@@ -186,36 +232,36 @@ read -n 1 -s -r -p "Please manually update any crate version not automatically h
 # Because the user might have updated the version numbers manually, we need to
 # parse them again before updating cross-dependencies
 
-local versionMap
-local crateVersion
-local crateName
+local version_map
+local crate_version
+local crate_name
 
-for $crate in $allCrates; do
-    readCrateVersion "$crate" crateVersionArray
-    crateName=$(tomlq -r .package.name "$crate/Cargo.toml")
-    versionMap[$crateName]=$(printVersionArray "${crateVersionArray[@]}")
+for $crate in $all_crates; do
+    read_crate_version "$crate" crate_version_array
+    crate_name=$(tomlq -r .package.name "$crate/Cargo.toml")
+    version_map[$crate_name]=$(print_version_array crate_version_array)
 done
 
-echo "Updating cross-dependencies..."
+report_progress "Updating cross-dependencies..."
 
-for $crate in $allCrates; do
-    bumpDependencies "crate" "$crate/Cargo.toml" versionMap
+for $crate in $all_crates; do
+    update_dependencies "crate" "$crate/Cargo.toml" version_map
 done
 
-bumpDependencies "workspace" "./Cargo.toml" versionMap
+update_dependencies "workspace" "./Cargo.toml" version_map
 
 # Patch workspace dependencies
 
-echo "Building and running checks..."
+report_progress "Building and running checks..."
 
 nix flake check
 
-echo "Pushing the release branch..."
+report_progress "Pushing the release branch..."
 
-git commit -m "Bump version to $(printVersionArray "${newWorkspaceVersion[@]}"
-git push -u origin $releaseBranch
+git commit -m "Bump version to $new_workspace_version"
+git push -u origin $release_branch
 
-echo "Saving current stable to stable-local-save..."
+report_progress "Saving current \'stable\' branch to \'stable-local-save\'..."
 
 # Delete the branch if already present, but if not, don't fail
 git branch -D stable-local-save &>/dev/null || true
@@ -223,23 +269,22 @@ git checkout stable
 git branch stable-local-save
 
 git checkout stable
-git reset --hard $releaseBranch
+git reset --hard $release_branch
 git push --force-with-lease
 
-echo "If anything goes wrong from now on, you can restore the previous stable branch by resetting stable to stable-local-save"
+report_progress "If anything goes wrong from now on, you can restore the previous stable branch by resetting stable to stable-local-save"
 
 # Reset cleanup actions as creating and pushing the release branch was successful
-cleanupActions=()
+cleanup_actions=()
 
 echo "++ Release branch successfully pushed!"
-
 echo "++ Prepare the release to crates.io"
 
-local cratesToPublish=(core cli lsp/nls)
+local crates_to_publish=(core cli lsp/nls)
 
-echo "Removing \'nickel-lang-utils\' from dev-dependencies..."
+report_progress "Removing \'nickel-lang-utils\' from dev-dependencies..."
 
-for $crate in $cratesToPublish; do
+for $crate in $crates_to_publish; do
     # Remove `nickel-lang-utils` from `dev-dependencies` of released crates.
     # Indeed, `nickel-lang-utils` is only used for testing or benchmarking and
     # it creates a circular dependency. We just don't publish it and cut it off
@@ -250,7 +295,7 @@ for $crate in $cratesToPublish; do
     # try to delete `nickel-lang-utils` even if it might not be there
     tomlq -i 'del(.dev-dependencies."nickel-lang-utils")' "$crate/Cargo.toml"
     git add "$crate/Cargo.toml"
-    cleanupActions+=("git restore \"$crate/Cargo.toml\"")
+    cleanup_actions+=("git restore \"$crate/Cargo.toml\"")
 done
 
 # Cargo requires to commit changes, but we'll reset them later
@@ -258,54 +303,52 @@ git commit -m "[release.sh] Remove nickel-lang-utils from dev-dependencies"
 
 # We have had reproducibility issues before due to the fact that when installing
 # the version of say `nickel-lang-cli` from crates.io, Cargo doesn't pick the
-# current workspace file `Cargo.lock`, but regerates a new one. This can lead
-# to a local installation of `nickel-lang-cli` succeeding, but after
+# current workspace file `Cargo.lock`, but regenerates a fresh one. This can
+# lead to a local installation of `nickel-lang-cli` succeeding, but after
 # publication, the version on crates.io wouldn't install.
 #
 # This is a bit unsatisfactory, but a cheap way to have good faith that the
 # published version correctly builds and install is to temporarily delete
-# `Cargo.lock` and try to install them locally
-
-echo "Trying to install \'nickel-lang-cli\' and \'nickel-lang-lsp\' locally... [WARNING: this will override your local Nickel installation"
+# `Cargo.lock` and try to install the nickel crates locally
+report_progress "Trying to install \'nickel-lang-cli\' and \'nickel-lang-lsp\' locally... [WARNING: this will override your local Nickel installation]"
 
 rm -f ./Cargo.lock
 
-cleanupActions+=("git restore ./Cargo.lock")
+cleanup_actions+=("git restore ./Cargo.lock")
 
 cargo install --force --path ./cli
 cargo install --force --path ./lsp/nls
 
 git restore ./Cargo.lock
 
-echo "Successfully installed locally. Trying a dry run of cargo publish..."
+report_progress "Successfully installed locally. Trying a dry run of cargo publish..."
 
 cargo publish -p nickel-lang-core --dry-run
 cargo publish -p nickel-lang-cli --dry-run
 cargo publish -p nickel-lang-lsp --dry-run
 
-echo "Dry run successfully passed"
-
-confirmAction "Proceed with actual publication to crates.io ?"
+report_progress "Dry run successfully passed"
+confirm_proceed "Proceed with actual publication to crates.io ?"
 
 cargo publish -p nickel-lang-core
 cargo publish -p nickel-lang-cli
 cargo publish -p nickel-lang-lsp
 
-echo "Cleaning up..."
+report_progress "Cleaning up..."
 
 # Undo the previous commit removing `nickel-lang-utils` from dev-dependencies
 git reset --hard HEAD~
 
-cleanupActions=()
-
+cleanup_actions=()
 
 echo <<EOF
+++ SUCCESS
+++
 ++ Successfully published to crates.io
-++ SUCCEEDED
 ++
 ++ Now, you need to:
 ++
 ++ - Do the GitHub release
 ++ - Redeploy the website
 ++
-++ Please consult RELEASING.md for more details
+++ Please refer to RELEASING.md for more details
