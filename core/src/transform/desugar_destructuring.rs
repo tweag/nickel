f9e7e9d85b73bb15f36fe4c7ac9ab6da98be4a77
Yann Hamdaoui <yann.hamdaoui@tweag.io>
2024-06-12 11:16:02 +0200

    Better error than unmatched pattern for destructuring

--- core/src/transform/desugar_destructuring.rs
+++ core/src/transform/desugar_destructuring.rs
@@ -3,14 +3,19 @@
 //! Replace a let-binding with destructuring by simpler constructs.
 //!
 //! A destructuring `let pat> = <destr> in <body>` is rewritten to the pattern matching
-//! `<destr> |> match { <pat> => <body> }`.
+//! `<destr> |> match { <pat> => <body>, _ => %seq% (<body> | GeneratedContract) (UnmatchedError) }`.
 //!
 //! A destructuring function `fun <pat> => <body>` is rewritten to
 //! `fun x => let <pat> = x in <body>`, and then the inner let is recursively desugared.
+use crate::error::EvalError;
+use crate::position::TermPos;
+use crate::stdlib::internals;
+use crate::term::BinaryOp;
+use crate::term::UnaryOp;
 use crate::{
     identifier::LocIdent,
-    match_sharedterm,
-    term::{pattern::*, MatchBranch, MatchData, RichTerm, Term},
+    match_sharedterm, mk_app,
+    term::{make as mk_term, pattern::*, MatchBranch, MatchData, RichTerm, Term},
 };
 
 /// Entry point of the destructuring desugaring transformation.
@@ -52,16 +57,60 @@ pub fn desugar_let(pattern: Pattern, bound: RichTerm, body: RichTerm) -> Term {
     // the position of the match expression is used during error reporting, so we try to provide a
     // sensible one.
     let match_expr_pos = pattern.pos.fuse(bound.pos);
+    let fresh_id = LocIdent::fresh();
+
+    let default_error = RichTerm::new(
+        Term::RuntimeError(EvalError::NonExhaustiveMatch {
+            value: bound.clone(),
+            pos: match_expr_pos,
+        }),
+        TermPos::None,
+    );
+
+    let default_body = if let Some(contract) = pattern.elaborate_contract() {
+        mk_app!(
+            mk_term::op1(
+                UnaryOp::DeepSeq,
+                mk_app!(
+                    mk_term::op2(
+                        BinaryOp::ContractApply,
+                        contract.typ.contract().unwrap(),
+                        Term::Lbl(contract.label),
+                    ),
+                    Term::Var(fresh_id)
+                )
+            ),
+            default_error
+        )
+        .with_pos(match_expr_pos)
+    } else {
+        default_error
+    };
+
+    // The default branch, which tries to first apply the generated contract (and force it) to
+    // provide better error message, or fallback to the unmatched pattern error.
+    let default_branch = MatchBranch {
+        pattern: Pattern {
+            data: PatternData::Any(fresh_id),
+            pos: TermPos::None,
+            alias: None,
+        },
+        guard: None,
+        body: default_body,
+    };
 
     // `(match { <pat> => <body> }) <bound>`
     Term::App(
         RichTerm::new(
             Term::Match(MatchData {
-                branches: vec![MatchBranch {
-                    pattern,
-                    guard: None,
-                    body,
-                }],
+                branches: vec![
+                    MatchBranch {
+                        pattern,
+                        guard: None,
+                        body,
+                    },
+                    default_branch,
+                ],
             }),
             match_expr_pos,
         ),
