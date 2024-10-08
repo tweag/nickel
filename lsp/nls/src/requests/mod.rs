a3d41ec326d1c5668f8ecf01210920c8b3972f67
Joe Neeman <joe.neeman@tweag.io>
2024-10-08 22:13:30 +0700

    Remove the option for an external formatter in nls

--- lsp/nls/src/requests/mod.rs
+++ lsp/nls/src/requests/mod.rs
@@ -1,12 +1,6 @@
 pub mod completion;
+pub mod formatting;
 pub mod goto;
 pub mod hover;
 pub mod rename;
 pub mod symbols;
-
-#[cfg(feature = "format")]
-pub mod formatting;
-
-#[cfg(not(feature = "format"))]
-#[path = "formatting_external.rs"]
-pub mod formatting;
