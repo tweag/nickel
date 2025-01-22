b351108844f514ce136bb1355d631d524612fd49
Joe Neeman <joe.neeman@tweag.io>
2025-01-22 15:34:18 +0700

    Try including the generated grammar in-tree

--- core/build.rs
+++ core/build.rs
@@ -1,8 +1,28 @@
+use std::path::{Path, PathBuf};
+
 fn main() {
-    lalrpop::Configuration::new()
-        .use_cargo_dir_conventions()
-        .process_file("src/parser/grammar.lalrpop")
-        .unwrap();
+    // Lalrpop is slow to generate grammars, so to make things easier for
+    // downstream users we check in a generated grammar.rs file. This is a bit
+    // awkward when we want to work on the grammar, though, so the build script
+    // will autogenerate a new grammar (in $OUT_DIR, so as not to mess up the
+    // source tree) whenever the generated grammar is missing from the source
+    // tree. Thus, when working on the grammar you just delete
+    // `src/parser/generated/grammar.rs` and the build script will rebuild it
+    // for you whenever necessary. When you're done working on the grammar,
+    // copy the generated file back into `src/parser/generated/grammar.rs`.
+    let checked_in_grammar_path = Path::new("src/parser/generated/grammar.rs");
+    let out_dir = PathBuf::from(std::env::var_os("OUT_DIR").expect("missing OUT_DIR variable"));
+    let out_parser_dir = out_dir.join("parser");
+    std::fs::create_dir_all(&out_parser_dir).expect("failed to create $OUT_DIR/parser");
+
+    // If we fail to link the generated grammar into $OUT_DIR, it probably didn't exist.
+    // We'll just pretend that's what happened, and try to generate a fresh grammar.
+    if std::fs::hard_link(checked_in_grammar_path, out_parser_dir.join("grammar.rs")).is_err() {
+        lalrpop::Configuration::new()
+            .use_cargo_dir_conventions()
+            .process_file("src/parser/grammar.lalrpop")
+            .unwrap();
+    }
 
     #[cfg(feature = "nix-experimental")]
     {
