0b8a27f823e1500058dcf3e539a517662f9e3a2a
Yann Hamdaoui <yann.hamdaoui@tweag.io>
2025-02-18 10:52:43 +0100

    Format merged review suggestion

--- core/src/bytecode/ast/mod.rs
+++ core/src/bytecode/ast/mod.rs
@@ -738,7 +738,9 @@ impl AstAlloc {
 
     /// Return the current number of allocated bytes.
     pub fn allocated_bytes(&self) -> usize {
-        self.generic_arena.allocated_bytes() + self.number_arena.len() * std::mem::size_of::<Number>() + self.error_arena.len() * std::mem::size_of::<ParseError>()
+        self.generic_arena.allocated_bytes()
+            + self.number_arena.len() * std::mem::size_of::<Number>()
+            + self.error_arena.len() * std::mem::size_of::<ParseError>()
     }
 
     /// Allocates an AST component in the arena.
