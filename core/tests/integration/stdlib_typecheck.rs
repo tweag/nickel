e3eb24fcd76bccd1d2b389bf442c35040907217c
Yann Hamdaoui <yann.hamdaoui@tweag.io>
2025-02-25 16:57:44 +0100

    Fix stdlib typecheck integration test

--- core/tests/integration/stdlib_typecheck.rs
+++ core/tests/integration/stdlib_typecheck.rs
@@ -1,9 +1,9 @@
 use assert_matches::assert_matches;
-use nickel_lang_core::cache::{Caches, ErrorTolerance};
+use nickel_lang_core::cache::Caches;
 
 #[test]
 fn stdlib_typecheck() {
-    let mut cache = Caches::new(ErrorTolerance::Strict);
+    let mut cache = Caches::new();
     assert_matches!(cache.load_stdlib(), Ok(_));
     assert_matches!(cache.typecheck_stdlib(), Ok(_));
 }
