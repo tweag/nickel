2d5a32ea5323d49422973c6d57adeb98a9447b0b
Joe Neeman <joe.neeman@tweag.io>
2025-02-25 08:57:39 +0700

    Fix one test

--- package/tests/up_to_date.rs
+++ package/tests/up_to_date.rs
@@ -1,6 +1,6 @@
 use nickel_lang_core::identifier::Ident;
 use nickel_lang_git::Target;
-use nickel_lang_package::Dependency;
+use nickel_lang_package::{snapshot::Snapshot, Dependency};
 use tempfile::TempDir;
 
 mod util;
@@ -13,9 +13,9 @@ fn empty_up_to_date() {
     let manifest = ManifestBuilder::default().with_dir(pkg_dir.path()).build();
     let (_cache_dir, config) = test_config();
 
-    let (lock, _snap) = manifest.lock(config).unwrap();
+    let (lock, resolution) = manifest.lock(config).unwrap();
 
-    assert!(manifest.is_lock_file_up_to_date(&lock));
+    assert!(manifest.is_lock_file_up_to_date(&resolution.snapshot, &lock));
 }
 
 #[test]
@@ -28,24 +28,25 @@ fn git_dep_up_to_date() {
         .build();
     let (_cache_dir, config) = test_config();
 
-    let (lock, _snap) = manifest.lock(config.clone()).unwrap();
+    let (lock, resolution) = manifest.lock(config.clone()).unwrap();
 
-    assert!(manifest.is_lock_file_up_to_date(&lock));
+    assert!(manifest.is_lock_file_up_to_date(&resolution.snapshot, &lock));
 
     // If we modify the git dep, it will still count as up-to-date.
     add_git_file(&git_dir, "main.ncl");
-    assert!(manifest.is_lock_file_up_to_date(&lock));
+    let snap = Snapshot::new(config.clone(), pkg_dir.path(), &manifest).unwrap();
+    assert!(manifest.is_lock_file_up_to_date(&snap, &lock));
 
     // If we modify the git spec in the manifest, it won't be up-to-date anymore.
     let Some(Dependency::Git(dep)) = manifest.dependencies.get_mut(&Ident::new("dep")) else {
         unreachable!()
     };
     dep.target = Target::Branch("master".to_owned());
-    assert!(!manifest.is_lock_file_up_to_date(&lock));
+    assert!(!manifest.is_lock_file_up_to_date(&snap, &lock));
 
     // Re-lock, and it will be up-to-date again.
-    let (lock, _snap) = manifest.lock(config).unwrap();
-    assert!(manifest.is_lock_file_up_to_date(&lock));
+    let (lock, resolution) = manifest.lock(config).unwrap();
+    assert!(manifest.is_lock_file_up_to_date(&resolution.snapshot, &lock));
 }
 
 #[test]
@@ -57,10 +58,13 @@ fn path_dep_up_to_date() {
         .with_path_dep("dep", dep_dir.path())
         .build();
     let (_cache_dir, config) = test_config();
-    let (lock, _snap) = manifest.lock(config.clone()).unwrap();
+    let (lock, resolution) = manifest.lock(config.clone()).unwrap();
 
     // Path deps are always up to date.
-    assert!(manifest.is_lock_file_up_to_date(&lock));
+    assert!(manifest.is_lock_file_up_to_date(&resolution.snapshot, &lock));
+    // FIXME: put a valid manifest here
     std::fs::write(dep_dir.path().join("Nickel-pkg.ncl"), "hi").unwrap();
-    assert!(manifest.is_lock_file_up_to_date(&lock));
+
+    let snap = Snapshot::new(config.clone(), pkg_dir.path(), &manifest).unwrap();
+    assert!(manifest.is_lock_file_up_to_date(&snap, &lock));
 }
