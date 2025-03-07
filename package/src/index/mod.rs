d92afcf8474386089e49f0cb77e948129df70857
jneem <joeneeman@gmail.com>
2025-03-07 09:06:43 +0700

    Update package/src/index/mod.rs
    Co-authored-by: Yann Hamdaoui <yann.hamdaoui@tweag.io>

--- package/src/index/mod.rs
+++ package/src/index/mod.rs
@@ -340,7 +340,7 @@ impl Id {
 impl std::fmt::Display for Id {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Id::Github { org, name } => write!(f, "github/{org}/{name}"),
+            Id::Github { org, name } => write!(f, "github:{org}/{name}"),
         }
     }
 }
